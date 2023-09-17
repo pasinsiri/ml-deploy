f5a7e389cf6a4203057636a51b65ecec06a8ac3a
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-17 20:46:45 +0700

    add model training

--- train_model.py
+++ train_model.py
@@ -1,8 +1,12 @@
 # Script to train machine learning model.
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
+import logging
 from functions.preprocessing import process_data
+
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.ensemble import GradientBoostingClassifier
 
 # Add code to load in the data.
 data = pd.read_csv('./data/census_income/adult.data', header=None)
@@ -34,5 +38,13 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False
+)
 
 # Train and save a model.
+cv = KFold(n_splits=10, shuffle=True, random_state=42)
+clf = GradientBoostingClassifier(n_estimators=50)
+clf.fit(X_train, y_train)
+scores = cross_val_score(clf, X_train, y_train, scoring='accuracy', cv=cv)
+logging.info(f'Average accuracy from cross-validation = {np.mean(scores)}')
