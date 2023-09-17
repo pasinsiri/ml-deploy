58b3f11c9eff941c58e016f46fede1f0a16d0f83
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-17 20:38:56 +0700

    refactor

--- train_model.py
+++ train_model.py
@@ -30,10 +30,11 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
 
 # Train and save a model.
