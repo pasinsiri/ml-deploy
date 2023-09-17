db5016b0a83857d2f683c1e756a2524643482346
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-17 21:30:17 +0700

    add skipinitialspace to read_csv

--- train_model.py
+++ train_model.py
@@ -6,20 +6,11 @@ import logging
 import joblib
 from functions.preprocessing import process_data
 
-from sklearn.model_selection import KFold, cross_val_score
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.ensemble import GradientBoostingClassifier
 
 # Add code to load in the data.
-train_data = pd.read_csv('./data/census_income/adult.data', header=None)
-test_data = pd.read_csv('./data/census_income/adult.test', header=None, skiprows=1)
-
-# Get column names
-meta = pd.read_table('./data/census_income/adult.names', header=None)
-col_list = meta.iloc[93:][0].apply(lambda x: x.split(':')[0]).values.tolist()
-col_list.append('salary')
-train_data.columns = col_list
-test_data.columns = col_list
-
+raw_df = pd.read_csv('./data/census.csv', skipinitialspace=True)
 
 cat_features = [
     "workclass",
@@ -33,12 +24,7 @@ cat_features = [
 ]
 
 X_train, y_train, encoder, lb = process_data(
-    train_data, categorical_features=cat_features, label="salary", training=True
-)
-
-# Process the test data with the process_data function.
-X_test, y_test, encoder, lb = process_data(
-    test_data, categorical_features=cat_features, label="salary", training=False
+    raw_df, categorical_features=cat_features, label="salary", training=True
 )
 
 # Train and save a model.
@@ -50,5 +36,3 @@ logging.info(f'Average accuracy from cross-validation on training data = {np.mea
 joblib.dump(clf, 'model/model.joblib')
 joblib.dump(encoder, 'model/encoder.joblib')
 joblib.dump(lb, 'model/lb.joblib')
-
-# Evaluate model on test data
