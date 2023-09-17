ad890c814e130cfadbe344d0f078e623dd28855a
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-17 19:57:58 +0700

    gather column names from meta file

--- train_model.py
+++ train_model.py
@@ -7,8 +7,15 @@ import pandas as pd
 import numpy as np
 
 # Add code to load in the data.
-raw_adult_data = pd.read_csv('./data/census_income/adult.data', header=None)
-raw_adult_test = pd.read_csv('./data/census_income/adult.test', header=None, skiprows=1)
+data = pd.read_csv('./data/census_income/adult.data', header=None)
+test_data = pd.read_csv('./data/census_income/adult.test', header=None, skiprows=1)
+
+# Get column names
+meta = pd.read_table('./data/census_income/adult.names', header=None)
+col_list = meta.iloc[93:][0].apply(lambda x: x.split(':')[0]).values.tolist()
+col_list.append('class')
+data.columns = col_list
+test_data.columns = col_list
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
