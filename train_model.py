86cd8e28c575d3bd430b5d478af8675ff31d86d2
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-17 19:55:22 +0700

    add load data

--- train_model.py
+++ train_model.py
@@ -3,8 +3,12 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import pandas as pd
+import numpy as np
 
 # Add code to load in the data.
+raw_adult_data = pd.read_csv('./data/census_income/adult.data', header=None)
+raw_adult_test = pd.read_csv('./data/census_income/adult.test', header=None, skiprows=1)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
