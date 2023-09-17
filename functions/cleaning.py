fc21506f52e12e7e196738543d80fbf58414051b
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-17 21:30:32 +0700

    initialize cleaning.py

---
+++ functions/cleaning.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def get_categorical_columns():
+    cat_cols = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    return cat_cols
+
+def clean_data(raw_df):
+    df = raw_df.copy()
+
+    # clean categorical columns
+    cat_cols = get_categorical_columns()
+    for c in cat_cols:
+        df[c] = df[c].str.strip()
+        df[c] = df[c].replace('?', None)
+
+    return df
