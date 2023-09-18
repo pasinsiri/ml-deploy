410dcc82e034fe488f2fd7f2dabc294ee033ed2e
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-18 21:38:41 +0700

    refactor model_evaluation_test.py

--- functions/model_evaluation_test.py
+++ functions/model_evaluation_test.py
@@ -15,6 +15,7 @@ def data():
     df = pd.read_csv('data/census.csv')
     return df
 
+
 def test_inference_above():
     """
     Check inference performance for case salary >50k
@@ -23,17 +24,45 @@ def test_inference_above():
     encoder = load("data/model/encoder.joblib")
     lb = load("data/model/lb.joblib")
 
-    test_case_above = [40, 'Private', 193524, 'Doctorate', 16, 'Married-civ-spouse', 'Prof-specialty', 
-                       'Husband', 'White', 'Male', 0, 0, 60, 'United-States']
-    test_case_above_df = pd.DataFrame(test_case_above, 
-                                      columns=['age', 'workclass', 'fnlgt', 'education', 'education-num', 
-                                               'marital-status', 'occupation', 'relationship', 'race', 'sex', 
-                                               'capital-gain', 'capital-loss', 'hours-per-week', 'native-country'])
+    test_case_above = [
+        40,
+        'Private',
+        193524,
+        'Doctorate',
+        16,
+        'Married-civ-spouse',
+        'Prof-specialty',
+        'Husband',
+        'White',
+        'Male',
+        0,
+        0,
+        60,
+        'United-States']
+    test_case_above_df = pd.DataFrame(
+        test_case_above,
+        columns=[
+            'age',
+            'workclass',
+            'fnlgt',
+            'education',
+            'education-num',
+            'marital-status',
+            'occupation',
+            'relationship',
+            'race',
+            'sex',
+            'capital-gain',
+            'capital-loss',
+            'hours-per-week',
+            'native-country'])
 
-    x, _, _, _ = process_data(test_case_above_df, cat_cols=get_categorical_columns(), encoder=encoder, lb=lb, training=False)
+    x, _, _, _ = process_data(test_case_above_df, cat_cols=get_categorical_columns(
+    ), encoder=encoder, lb=lb, training=False)
     pred = inference(model, x)
     y = lb.inverse_transform(pred)[0]
     assert y == ">50K"
+
 
 def test_inference_below():
     """
@@ -43,14 +72,41 @@ def test_inference_below():
     encoder = load("data/model/encoder.joblib")
     lb = load("data/model/lb.joblib")
 
-    test_case_below = [31, 'Private', 507875, '9th', 5, 'Married-civ-spouse', 'Machine-op-inspct', 
-                       'Husband', 'White', 'Male', 0, 0, 43, 'United-States']
-    test_case_below_df = pd.DataFrame(test_case_below, 
-                                      columns=['age', 'workclass', 'fnlgt', 'education', 'education-num', 
-                                               'marital-status', 'occupation', 'relationship', 'race', 'sex', 
-                                               'capital-gain', 'capital-loss', 'hours-per-week', 'native-country'])
+    test_case_below = [
+        31,
+        'Private',
+        507875,
+        '9th',
+        5,
+        'Married-civ-spouse',
+        'Machine-op-inspct',
+        'Husband',
+        'White',
+        'Male',
+        0,
+        0,
+        43,
+        'United-States']
+    test_case_below_df = pd.DataFrame(
+        test_case_below,
+        columns=[
+            'age',
+            'workclass',
+            'fnlgt',
+            'education',
+            'education-num',
+            'marital-status',
+            'occupation',
+            'relationship',
+            'race',
+            'sex',
+            'capital-gain',
+            'capital-loss',
+            'hours-per-week',
+            'native-country'])
 
-    x, _, _, _ = process_data(test_case_below_df, cat_cols=get_categorical_columns(), encoder=encoder, lb=lb, training=False)
+    x, _, _, _ = process_data(test_case_below_df, cat_cols=get_categorical_columns(
+    ), encoder=encoder, lb=lb, training=False)
     pred = inference(model, x)
     y = lb.inverse_transform(pred)[0]
     assert y == "<=50K"
