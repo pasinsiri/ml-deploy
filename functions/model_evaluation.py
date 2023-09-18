29adcf6ce36439cf68afb69302ee7e180007ccff
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-18 20:26:00 +0700

    initialize model_evaluation

---
+++ functions/model_evaluation.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import logging
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from cleaning import get_categorical_columns, clean_data
+from preprocessing import process_data
+# from joblib import load
+# import src.common_functions
+
+def calculate_f1(y_true, y_pred):
+    """validate the model using F1 score
+
+    Args:
+        y_true: known labels (binarized)
+        y_pred: predicted labels (binarized)
+
+    Returns:
+        float: F1 score
+    """
+    f1 = f1_score(y_true, y_pred)
+    return f1
+
+def inference(model, x):
+    """run model inference and return predicted values
+
+    Args:
+        model: an ML model
+        x: input data to be predicted
+
+    Returns:
+        np.array: predicted values
+    """
+
+    y_preds = model.predict(x)
+    return y_preds
+
+# def check_score():
+def slicing():
+    """
+    slice the data by features
+    """
+    df = pd.read_csv("data/prepared/census.csv")
+    _, test = train_test_split(df, test_size=0.20)
+
+    trained_model = joblib.load("data/model/model.joblib")
+    encoder = joblib.load("data/model/encoder.joblib")
+    lb = joblib.load("data/model/lb.joblib")
+
+    cat_cols = get_categorical_columns()
+    slice_values = []
+
+    for cat in cat_cols:
+        for cls in test[cat].unique():
+            sliced_df = test[test[cat] == cls]
+            x_test, y_test, _, _ = process_data(sliced_df, categorical_features=cat_cols, 
+                                                label="salary", encoder=encoder, lb=lb, training=False)
+
+            y_pred = trained_model.predict(x_test)
+            f1 = calculate_f1(y_test, y_pred)
+            line = f'[{cat} = {cls}]: F1 Score = {f1}'
+            logging.info(line)
+            slice_values.append(line)
+
+    with open('model/slice_output.txt', 'w') as out:
+        for slice_value in slice_values:
+            out.write(slice_value + '\n')
