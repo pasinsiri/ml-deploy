5795754b16f7e5777702accce1b9a66ef8df8797
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-18 22:27:02 +0700

    add clean column names

--- functions/cleaning.py
+++ functions/cleaning.py
@@ -20,10 +20,12 @@ def get_categorical_columns():
 def clean_data(raw_df):
     df = raw_df.copy()
 
+    # trim column names
+    df.columns = [c.strip() for c in df.columns]
+
     # clean categorical columns
     cat_cols = get_categorical_columns()
     for c in cat_cols:
-        df[c] = df[c].str.strip()
         df[c] = df[c].replace('?', None)
 
     return df.dropna()
